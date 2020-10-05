4eceb6f390b1744968e5ed3256bbc3f540d55ebc
Dinesh B S <dineshbs444@gmail.com>
2020-10-05 18:09:13 +0530

    Added 3rd Bottom nav "About"!

---
+++ app/src/main/java/com/benrostudios/libground/AboutFragment.java
@@ -0,0 +1,28 @@
+package com.benrostudios.libground;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+    public static AboutFragment newInstance(String param1, String param2) {
+        AboutFragment fragment = new AboutFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
